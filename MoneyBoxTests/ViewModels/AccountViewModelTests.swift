0d52ced9c51f7309d840c263bd0a68e37e95dca9
Thecafremo <jorgepardopeset@gmail.com>
2023-09-19 18:28:37 +0100

    Add the `AccountViewModelTests` file.

---
+++ MoneyBoxTests/ViewModels/AccountViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  AccountViewModelTests.swift
+//  MoneyBoxTests
+//
+//  Created by Thecafremo on 19/09/2023.
+//
+
+import XCTest
+
+final class AccountViewModelTests: XCTestCase {
+
+    // All Models and `ViewModel` should have tests. I've only implemented one, as it showcases how the rest would be implemented. 
+}
